e8e8927c6513fea6c5646b41709b756cc2f00b67
Ezra <ezra@usefuliftrue.com>
2017-08-16 14:01:17 -0600

    Display the right number of days + Stories for 2009 to 2017 + Day and Week components

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,20 +1,46 @@
 import React from "react"
+import moment from "moment"
 
-const Day = ({ date }) => {
+// display a single day
+const Day = ({ date, offset }) => {
   return (
     <div>
-      {date}
+      Day {offset} {date.format()}
     </div>
   )
 }
 
-// a 53x7 grid, represting the calendar year, YYYY.
+// display `length` consecutive days in a div, starting from `basedate`
+const Week = ({ basedate, index, length }) => {
+  let days = Array(length).fill(0)
+  return (
+    <div>
+      Week {index}
+      {days.map((_, ii) =>
+        <Day date={basedate.clone().add(ii, "days")} offset={ii} />
+      )}
+    </div>
+  )
+}
+
+// a ~53x7 grid, represting the calendar year, YYYY.
 // the first visible date is the first sunday <= YYYY-jan-01
 // the last visible date is *always* YYYY-dec-31
 const Calendar = ({ year }) => {
+  const end = moment(`${year}-dec-31`, "YYYY-MMM-DD")
+  const start = end.clone().subtract(1, "year").weekday(0)
+  let days = end.diff(start, "days") + 1
+  let weeks = Array(Math.ceil(days / 7)).fill(0)
   return (
     <div>
-      {year}
+      {days}
+      {weeks.map((_, ii) =>
+        <Week
+          basedate={start.clone().add(ii, "weeks")}
+          length={(ii + 1) * 7 < days ? 7 : days % 7}
+          index={ii}
+        />
+      )}
     </div>
   )
 }
